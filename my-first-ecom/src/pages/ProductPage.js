dd38a5cdb7391e2eb236c6db6aab5b184beda731
Mare94 <mn-sand@online.no>
2024-01-06 21:27:43 +0100

    product page

---
+++ my-first-ecom/src/pages/ProductPage.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Link, useParams } from 'react-router-dom';
+
+const url = 'https://api.noroff.dev/api/v1/online-shop/'
+
+function Home() {
+    return <div>Home</div>
+};
+
+function Post() {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    let { id } = useParams();
+
+    useEffect(()=>{
+        async function getData(url) {
+            try {
+                setIsLoading(true);
+                setIsError(false);
+
+                const reponse = await fetch(url);
+                const json = await reponse.json();
+
+                setData(json);
+            } catch(error) {
+                console.log(error)
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        getData(`https://api.noroff.dev/api/v1/online-shop/${id}`);
+    }, [id]);
+
+    if(isLoading || !data){
+        return <div>Loading</div>
+    }
+    if(isError) {
+        return <div>Error</div>
+    }
+
+    console.log(data);
+
+    return (
+        <div>
+            <div>{data.id}</div>
+            <div>ID: {data.id}</div>
+            <div>Title: {data.title}</div>
+            <div><p>{data.body}</p></div>
+            <div>Price: {data.price}</div>
+        </div>
+    );
+
+
+}
