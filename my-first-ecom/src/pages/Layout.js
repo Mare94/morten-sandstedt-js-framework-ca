77d25f3d3ffebe18cc3900d68812c1fdd41c5af0
Mare94 <mn-sand@online.no>
2024-01-06 18:09:20 +0100

    created layout

---
+++ my-first-ecom/src/pages/Layout.js
@@ -0,0 +1,50 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <>
+        <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/productpage">Products</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Outlet />
+        </>
+    )
+}
+
+const Header = () => {
+    return (
+        <>
+        <header>
+            <div>Header</div>
+            <Nav />
+        </header>
+        </>
+    )
+}
+
+const Footer = () => {
+    return <footer>Website footer</footer>
+}
+
+const Layout = () => {
+    return (
+        <>
+        <Header />
+        <Outlet />
+        <Footer />
+        </>
+    )
+}
+
+export default Layout;
